666df36fa8fcf447424244f874d6d9defe5d62f7
Ranjan35gupta <Ranjan35gupta@gmail.com>
2023-05-03 02:31:37 +0530

    all changes is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+
+  const [count,setCount]=useState()
+  useEffect( ()=>{
+    
+    async function changeDog(){
+      try{
+      const response =await fetch("https://dog.ceo/api/breeds/image/random")
+      const data = await response.json()
+      setCount(data.message)}
+
+      catch(error){
+        console.log("it is error",error)
+      }
+  }
+       changeDog()
+     },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className>
+      <img src={count} className='changeImage' style={{height:"300px",
+    width:"300px"}}/>
+      
     </div>
   );
 }
---
+++ src/components/RefreshImage.js
@@ -0,0 +1,8 @@
+function RefreshImager(){
+    return(
+
+        <div>
+            
+        </div>
+    )
+}
