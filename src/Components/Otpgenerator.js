e9cffa8b260ee4501b1e526c3c26b20ac3164ab0
Ranjan35gupta <Ranjan35gupta@gmail.com>
2023-05-05 01:47:23 +0530

    i updated the changes

--- src/Components/Otpgenerator.js
+++ src/Components/Otpgenerator.js
@@ -22,7 +22,7 @@ export function OtpGenerator() {
             data: {
                 mobile: mobileNumber
             }
-        })-
+        })
             .then(response => response )
             .catch(error => error)
 
