06e1b593df76b9a864712ef380b962401ab3ebfc
Ranjan35gupta <Ranjan35gupta@gmail.com>
2023-05-28 10:28:29 +0530

    i updated the changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import Movies from './components/organisms/Home'
+import Cart from './components/molecules/cart/Cart';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './components/redux/store'
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <div className="linkcontainer">
+        <Link style={{textDecoration:"none"}} to="/cart">Cart</Link>
+      <Link  style={{textDecoration:"none"}} to="/">Home</Link></div>
+      
+      <Provider store={store}>
+      <Routes>
+       <Route path='/' element={<Movies/>}   />
+       <Route path='/cart' element={<Cart/>}   />
+        
+        <Route/>
+      </Routes>
+      </Provider>
+      </BrowserRouter>
     </div>
   );
 }
